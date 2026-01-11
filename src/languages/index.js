6ae8c107a8c81d047ec8de5dce853e68c1467192
simon <ertel.simon+github@gmail.com>
2026-01-11 20:29:21 +0100

    ajout traductions

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,9 +1,25 @@
 import fr from './fr.json';
 import en from './en.json';
+import de from './de.json';
+import es from './es.json';
+import sl from './sl.json';
+import lv from './lv.json';
+import it from './it.json';
+import sv from './sv.json';
+import hu from './hu.json';
+import uk from './uk.json';
 
-export const dictionaryList = { en, fr };
+export const dictionaryList = { en, fr, de, es, sl, lv, it, sv, hu, uk };
 
 export const languageOptions = {
   en: 'English',
-  fr: 'Français'
+  fr: 'Français',
+  de: 'Deutsch',
+  es: 'Español',
+  sl: 'Slovenščina',
+  lv: 'Latviešu',
+  it: 'Italiano',
+  sv: 'Svenska',
+  hu: 'Magyar',
+  uk: 'Українська'
 };
